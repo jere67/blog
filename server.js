d17dd5f882fed1e99cf8c411534372f39be3a45d
Jeremy Moon <jeremymoon567@gmail.com>
2024-07-11 13:07:31 +0900

    change API url for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "http://localhost:4000";
+const API_URL = "https://blogmoon.onrender.com";
 
 app.use(express.static("public")); // Use for client file
 
